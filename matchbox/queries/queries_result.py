c3b434c6a71ff581227974d9cf192fc5146d6bce
Maciej Gębarski <mgebarski@gmail.com>
2019-04-29 23:19:58 +0200

    refactor result

--- matchbox/queries/queries_result.py
+++ matchbox/queries/queries_result.py
@@ -10,24 +10,27 @@ class QueryResultWrapper(object):
             if attr in instance._meta.fields:
                 field = instance._meta.fields[attr]
                 if isinstance(field, fields.ReferenceField):
-                    if not value:
-                        return None
-                    db_val = value.get()
-                    if not db_val.exists:
-                        raise error.ReferenceCollectionObjectDoesNotExist(
-                            '{}/{}'.format(
-                                field.ref_model.collection_name(),
-                                value.id
-                            )
-                        )
-                    setattr(
-                        instance,
-                        attr,
-                        QueryResultWrapper.model_from_dict(
-                            field.ref_model, db_val
-                        )
-                    )
+                    val = ReferenceFieldWrapper.model_from_dict(field, value)
                 else:
-                    setattr(instance, attr, field.python_value(value))
+                    val = field.python_value(value)
+                setattr(instance, attr, val)
         instance.id = row_dict.id
         return instance
+
+
+class ReferenceFieldWrapper(object):
+    @classmethod
+    def model_from_dict(cls, field, value):
+        if not value:
+            return None
+        db_val = value.get()
+        if not db_val.exists:
+            raise error.ReferenceCollectionObjectDoesNotExist(
+                '{}/{}'.format(
+                    field.ref_model.collection_name(),
+                    value.id
+                )
+            )
+        return QueryResultWrapper.model_from_dict(
+            field.ref_model, db_val
+        )
