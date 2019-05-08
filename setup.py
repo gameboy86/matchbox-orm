5c7caeeb6c2aaa91b756677a91e0369969cb40f0
Maciej Gębarski <mgebarski@gmail.com>
2019-05-08 23:54:41 +0200

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='matchbox-orm',
-    version='0.1.0',
+    version='0.1',
     description='matchbox is orm package for google Cloud Firestore',
     long_description=long_description,
     long_description_content_type='text/markdown',
